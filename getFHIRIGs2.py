55d93ee89d9f6902a1ea6840921796bb9dbc2f55
Cameron Farzaneh <cfarzaneh3@gatech.edu>
2021-04-13 07:09:52 -0400

    Adding new file getFHIRIGs2 to fetch all IGs from qas.json

---
+++ getFHIRIGs2.py
@@ -0,0 +1,44 @@
+import json
+import dateutil.parser
+import os
+import shutil
+import urllib.request
+from tqdm import tqdm
+
+with open('qas.json') as f:
+  data = json.load(f) #Load the JSON
+
+sorted_data = sorted(data, key=lambda x: dateutil.parser.parse(x['date']), reverse=True) #Sort by most recent
+
+seen = set()
+igs = []
+
+for ig in data:
+  if ig["package-id"] not in seen: #Take the most recent version of the package, skip duplicates
+    seen.add(ig["package-id"])
+    igs.append(ig)
+
+if os.path.exists('output'):
+    print("Warning: Output folder with downloads already exists.")
+    user = input("Would you like to delete and download again? Y/N: ")
+    if user == "Y" or user == "y":
+      shutil.rmtree('output')
+    else:
+      print("Aborted")
+      quit()
+    
+os.makedirs('output')
+
+for ig in tqdm(igs):
+  baseURL = "https://build.fhir.org/ig/"
+  packageURL = ig['repo'].replace("qa.json","package.tgz")
+
+  try:
+    filename = "output/" + ig["name"] + '.tgz'
+  except:
+    filename = "output/" + ig['package-id'] + '.tgz'
+
+  try:
+    urllib.request.urlretrieve(baseURL+packageURL, filename)
+  except:
+    print(ig["name"] + " could not be found.")
